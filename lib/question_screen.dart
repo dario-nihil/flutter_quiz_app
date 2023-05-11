99d5f8878892d8f50e02a86eafdc33f40ffac58c
dario-nihil <dario.nihil@gmail.com>
2023-05-11 10:30:12 +0200

    display buttons answer dynamically

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -11,24 +12,36 @@ class QuestionScreen extends StatefulWidget {
 
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
-  Widget build(BuildContext context) => SizedBox(
-        width: double.infinity,
+  Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             const Text(
               'The question...',
               style: TextStyle(
                 color: Colors.white,
               ),
+              textAlign: TextAlign.center,
             ),
             const SizedBox(
               height: 30,
             ),
-            AnswerButton('Answer 1', () {}),
-            AnswerButton('Answer 2', () {}),
-            AnswerButton('Answer 3', () {})
+            ...currentQuestion.answers.map(
+              (answer) => AnswerButton(
+                answer,
+                () {},
+              ),
+            )
           ],
         ),
-      );
+      ),
+    );
+  }
 }
